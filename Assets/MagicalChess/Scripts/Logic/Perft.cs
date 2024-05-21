eace5064763ebcea335c905f5de0db9c392badba
marcel-gerber <marcelgerber9@gmail.com>
2024-05-21 16:28:22 +0200

    Added Perft

---
+++ Assets/MagicalChess/Scripts/Logic/Perft.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Perft {
+
+    private Board _board;
+
+    public Perft(Board board) {
+        _board = board;
+    }
+
+    private int perft(int depth) {
+        List<Move> legalMoves = _board.GetPseudoLegalMoves();
+
+        if (depth == 1) {
+            return legalMoves.Count;
+        }
+
+        int nodes = 0;
+        foreach (Move move in legalMoves) {
+            _board.MakeMove(move);
+            nodes += perft(depth - 1);
+            _board.UnmakeMove(move);
+        }
+
+        return nodes;
+    }
+
+    public void Run(int depth) {
+        int nodes = perft(depth);
+        Debug.Log("Nodes: " + nodes);
+    }
+
+}
